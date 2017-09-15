09af39a0a1cd0a8c0b51c0b1cb6435c62def5845
James Giller <giller@bubblelab.com>
2017-09-15 17:53:56 +0900

    Change AprilTag msg to use PoseStamped.
    Previously, AprilTag had separate fields for x, y, z, roll, pitch, yaw.

--- src/april_tag_node.cpp
+++ src/april_tag_node.cpp
@@ -1,169 +1,172 @@
 //
 // adapted from ros example and april tag examples - palash
 //
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
+#include <cmath>
+
+#include <algorithm>
+
 #include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
 #include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <tf/transform_datatypes.h>
+
 #include <opencv2/highgui/highgui.hpp>
-#include "AprilTags/TagDetector.h"
-#include "AprilTags/Tag36h11.h"
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "april_tag/AprilTag.h" // rosmsg
 #include "april_tag/AprilTagList.h" // rosmsg
+#include "AprilTags/TagDetector.h"
+#include "AprilTags/Tag36h11.h"
 
+namespace
+{
+  const double PI  = 3.14159265358979;
+  const double TAU = 6.28318530717958;
+  const double NEG_TAU = -6.28318530717958;
 
-static const std::string OPENCV_WINDOW = "Image window";
-
-const double PI = 3.14159265358979323846;
-const double TWOPI = 2.0*PI;
-
-/**
- * Normalize angle to be within the interval [-pi,pi].
- */
-inline double standardRad(double t) {
-  if (t >= 0.) {
-    t = fmod(t+PI, TWOPI) - PI;
-  } else {
-    t = fmod(t-PI, -TWOPI) + PI;
+  /**
+   * Normalize angle to be within the interval [-pi,pi].
+   */
+  inline double standardRad(double t) {
+    if (t >= 0.) {
+      t = std::fmod(t+PI, TAU) - PI;
+    } else {
+      t = std::fmod(t-PI, NEG_TAU) + PI;
+    }
+    return t;
   }
-  return t;
-}
 
-/**
- * Convert rotation matrix to Euler angles
- */
-void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double& roll) {
-    yaw = standardRad(atan2(wRo(1,0), wRo(0,0)));
-    double c = cos(yaw);
-    double s = sin(yaw);
-    pitch = standardRad(atan2(-wRo(2,0), wRo(0,0)*c + wRo(1,0)*s));
-    roll  = standardRad(atan2(wRo(0,2)*s - wRo(1,2)*c, -wRo(0,1)*s + wRo(1,1)*c));
+  /**
+  * Convert rotation matrix to Euler angles
+  */
+  void rotationMatToEuler(const Eigen::Matrix3d &wRo, double &yaw, double &pitch, double &roll) {
+    yaw = standardRad(std::atan2(wRo(1,0), wRo(0,0)));
+    double c = std::cos(yaw);
+    double s = std::sin(yaw);
+    pitch = standardRad(std::atan2(-wRo(2,0), wRo(0,0)*c + wRo(1,0)*s));
+    roll  = standardRad(std::atan2(wRo(0,2)*s - wRo(1,2)*c, -wRo(0,1)*s + wRo(1,1)*c));
+  }
 }
-
 
 class AprilTagNode
 {
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  ros::Publisher tag_list_pub;
-  AprilTags::TagDetector tag_detector;
+  static const std::string OPENCV_WINDOW;
 
   // allow configurations for these:  
-  double camera_focal_length_x_px_; // late 2013 macbookpro retina = 700, 700
+  bool  show_debug_image_;
+  sensor_msgs::ImageConstPtr latest_image_;
+  double camera_focal_length_x_px_;
   double camera_focal_length_y_px_;
-  double camera_principal_point_x_px_; // 640x480 image = 320, 240 (middle)
+  double camera_principal_point_x_px_;
   double camera_principal_point_y_px_;
   double tag_size_m_;
-  bool  show_debug_image_;
 
+  AprilTags::TagDetector tag_detector_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  ros::Publisher tag_list_pub_;
+  
 public:
   AprilTagNode(ros::NodeHandle &nh, const AprilTags::TagCodes &tag_codes) : 
-    it_(nh), 
-    tag_detector(tag_codes)
+    tag_detector_(tag_codes),
+    it_(nh) 
   {
     // Subscrive to input video feed and publish output video feed
     image_sub_ = it_.subscribe("/camera/image_raw", 1, &AprilTagNode::imageCb, this);
-    image_pub_ = it_.advertise("/april_tag_debug/output_video", 1);
-    tag_list_pub = nh.advertise<april_tag::AprilTagList>("/april_tags", 100);
+    tag_list_pub_ = nh.advertise<april_tag::AprilTagList>("/april_tags", 100);
 
     // Use a private node handle so that multiple instances of the node can
     // be run simultaneously while using different parameters.
     ros::NodeHandle private_node_handle("~"); 
-    private_node_handle.param<double>("focal_length_x_px", camera_focal_length_x_px_, 700.0);
-    private_node_handle.param<double>("focal_length_y_px", camera_focal_length_y_px_, 700.0);
-    private_node_handle.param<double>("principal_point_x_px_", camera_principal_point_x_px_, 320.0);
-    private_node_handle.param<double>("principal_point_y_px_", camera_principal_point_y_px_, 240.0);
-
     private_node_handle.param<bool>("show_debug_image", show_debug_image_, false);
+
+    private_node_handle.param<double>("focal_length_x_px", camera_focal_length_x_px_, 700.0); // late 2013 macbookpro retina fx = 700
+    private_node_handle.param<double>("focal_length_y_px", camera_focal_length_y_px_, 700.0);
+    private_node_handle.param<double>("principal_point_x_px_", camera_principal_point_x_px_, 320.0); // 640x480 image = (320, 240) middle
+    private_node_handle.param<double>("principal_point_y_px_", camera_principal_point_y_px_, 240.0);
 
     double tag_size_cm;
     private_node_handle.param<double>("tag_size_cm", tag_size_cm, 2.9);
     tag_size_m_ = tag_size_cm / 100.0;
 
 
-    cout << "got focal length " << camera_focal_length_x_px_ << ", " << camera_focal_length_y_px_ << endl;
-    cout << "got principal point " << camera_principal_point_x_px_ << ", " << camera_principal_point_y_px_ << endl;
-    cout << "got tag size (in meters) " << tag_size_m_ << endl;
+    ROS_INFO_STREAM("got focal length " << camera_focal_length_x_px_ << ", " << camera_focal_length_y_px_);
+    ROS_INFO_STREAM("got principal point " << camera_principal_point_x_px_ << ", " << camera_principal_point_y_px_);
+    ROS_INFO_STREAM("got tag size (in meters) " << tag_size_m_);
 
-    if (show_debug_image_) {
+    if (show_debug_image_)
+    {
       cv::namedWindow(OPENCV_WINDOW);
     }
   }
 
   ~AprilTagNode()
   {
-    if (show_debug_image_) {
+    if (show_debug_image_)
+    {
      cv::destroyWindow(OPENCV_WINDOW);
     }
   }
 
-  april_tag::AprilTag convert_to_msg(AprilTags::TagDetection& detection) {
-    // recovering the relative pose of a tag:
-
-    // NOTE: for this to be accurate, it is necessary to use the
-    // actual camera parameters here as well as the actual tag size
-    // (m_fx, m_fy, m_px, m_py, m_tagSize)
-
-    Eigen::Vector3d translation;
-    Eigen::Matrix3d rotation;
-    detection.getRelativeTranslationRotation(tag_size_m_, 
-                                             camera_focal_length_x_px_, 
-                                             camera_focal_length_y_px_, 
-                                             camera_principal_point_x_px_, 
-                                             camera_principal_point_y_px_,
-                                             translation, 
-                                             rotation);
+  april_tag::AprilTag convert_to_msg(AprilTags::TagDetection &detection) {
+    Eigen::Matrix4d transformation_matrix = detection.getRelativeTransform(tag_size_m_, 
+                                                                           camera_focal_length_x_px_, 
+                                                                           camera_focal_length_y_px_, 
+                                                                           camera_principal_point_x_px_, 
+                                                                           camera_principal_point_y_px_);
+    Eigen::Vector3d translation = transformation_matrix.col(3).head(3);
+    Eigen::Matrix3d rotation = transformation_matrix.block(0, 0, 3, 3);
 
     Eigen::Matrix3d F;
     F <<
       1, 0,  0,
       0,  -1,  0,
       0,  0,  1;
-    Eigen::Matrix3d fixed_rot = F*rotation;
-    double yaw, pitch, roll;
-    wRo_to_euler(fixed_rot, yaw, pitch, roll);
-
+    Eigen::Matrix3d fixed_rot = F * rotation;
 
     april_tag::AprilTag tag_msg;
-
     tag_msg.id = detection.id;
     tag_msg.hamming_distance = detection.hammingDistance;
-    tag_msg.distance = translation.norm() * 100.0;
-    tag_msg.z = translation(0) * 100.0; // depth from camera
-    tag_msg.x = translation(1) * 100.0; // horizontal displacement (camera pov right = +ve)
-    tag_msg.y = translation(2) * 100.0; // vertical displacement
-    tag_msg.yaw = yaw;
-    tag_msg.pitch = pitch;
-    tag_msg.roll = roll;
+    tag_msg.distance = translation.norm();
+    tag_msg.stamped.header.stamp = latest_image_->header.stamp;
+    tag_msg.stamped.header.frame_id = latest_image_->header.frame_id;
+    tag_msg.stamped.pose.position.x = translation(0); 
+    tag_msg.stamped.pose.position.y = translation(1); 
+    tag_msg.stamped.pose.position.z = translation(2); 
+    double roll, pitch, yaw;
+    rotationMatToEuler(fixed_rot, yaw, pitch, roll);
+    tag_msg.stamped.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(roll, pitch, yaw);
     return tag_msg;
   }
-
 
   void processCvImage(cv_bridge::CvImagePtr cv_ptr) 
   {
     cv::Mat image_gray;
     cv::cvtColor(cv_ptr->image, image_gray, CV_BGR2GRAY);
-    vector<AprilTags::TagDetection> detections = tag_detector.extractTags(image_gray);
+    vector<AprilTags::TagDetection> detections = tag_detector_.extractTags(image_gray);
     vector<april_tag::AprilTag> tag_msgs;
 
-    for (int i=0; i<detections.size(); i++) {
-      detections[i].draw(cv_ptr->image);
+    for (int i=0; i < detections.size(); ++i)
+    {
+      if (show_debug_image_)
+      {
+        detections[i].draw(cv_ptr->image);
+      }
       tag_msgs.push_back(convert_to_msg(detections[i]));
     }
 
-    if(detections.size() > 0) { // take this out if you want absence notificaiton
+    if(detections.size() > 0)
+    { // take this out if you want absence notificaiton
       april_tag::AprilTagList tag_list;
       tag_list.april_tags = tag_msgs;
-      tag_list_pub.publish(tag_list);
+      tag_list_pub_.publish(tag_list);
     }
   }
 
-
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
+    latest_image_ = msg;
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
@@ -177,16 +180,16 @@ public:
 
     processCvImage(cv_ptr);
 
-    if (show_debug_image_) {
+    if (show_debug_image_)
+    {
       // Update GUI Window
       cv::imshow(OPENCV_WINDOW, cv_ptr->image);
       cv::waitKey(3);
     }
-
-    // Output modified video stream
-    //image_pub_.publish(cv_ptr->toImageMsg());
   }
 };
+
+const std::string AprilTagNode::OPENCV_WINDOW = "Image window";
 
 int main(int argc, char** argv)
 {
