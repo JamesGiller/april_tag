bda009588f99bd446ab863f7e133097f3d7a1895
James Giller <jamgiller@gmail.com>
2017-10-12 14:22:19 +0900

    Fetch camera intrinsics from published CameraInfo (#3)
    * Fetch camera intrinsics from published CameraInfo.
    
    If parameters for camera intrinsics aren't set, wait for a message on
    <camera namespace>/camera_info; Wait forever, or until node shutdown. Warn the user if intrinsics in CameraInfo message are all zero, but use the values anyway.
    
    Remove launch files for different cameras in favour of a generic launch file.

--- src/april_tag_node.cpp
+++ src/april_tag_node.cpp
@@ -8,6 +8,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <ros/ros.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/image_encodings.h>
 #include <tf/transform_datatypes.h>
 
@@ -72,28 +73,59 @@ public:
     tag_detector_(tag_codes),
     it_(nh) 
   {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1, &AprilTagNode::imageCb, this);
-    tag_list_pub_ = nh.advertise<april_tag::AprilTagList>("/april_tags", 100);
-
-    // Use a private node handle so that multiple instances of the node can
-    // be run simultaneously while using different parameters.
-    ros::NodeHandle private_node_handle("~"); 
-    private_node_handle.param<bool>("show_debug_image", show_debug_image_, false);
-
-    private_node_handle.param<double>("focal_length_x_px", camera_focal_length_x_px_, 700.0); // late 2013 macbookpro retina fx = 700
-    private_node_handle.param<double>("focal_length_y_px", camera_focal_length_y_px_, 700.0);
-    private_node_handle.param<double>("principal_point_x_px_", camera_principal_point_x_px_, 320.0); // 640x480 image = (320, 240) middle
-    private_node_handle.param<double>("principal_point_y_px_", camera_principal_point_y_px_, 240.0);
-
     double tag_size_cm;
-    nh.param<double>("tag_size_cm", tag_size_cm, 5.9);
+    if(!nh.getParam("tag_size_cm", tag_size_cm))
+    {
+      ROS_ERROR("Parameter 'tag_size_cm' is required but not set");
+      exit(1);
+    }
     tag_size_m_ = tag_size_cm / 100.0;
 
+    ros::NodeHandle private_node_handle("~");
+
+    std::string camera_ns;
+    if(!private_node_handle.getParam("camera_ns", camera_ns))
+    {
+      ROS_ERROR("Parameter 'camera_ns' is required but not set");
+      exit(1);
+    }
+
+    private_node_handle.param<double>("focal_length_x_px", camera_focal_length_x_px_, -1.);
+    private_node_handle.param<double>("focal_length_y_px", camera_focal_length_y_px_, -1.);
+    private_node_handle.param<double>("principal_point_x_px", camera_principal_point_x_px_, -1.);
+    private_node_handle.param<double>("principal_point_y_px", camera_principal_point_y_px_, -1.);
+
+    if(camera_focal_length_x_px_ < 0. || camera_focal_length_y_px_ < 0.
+       || camera_principal_point_x_px_ < 0. || camera_principal_point_y_px_ < 0.)
+    {
+      ROS_INFO("Camera intrinsics not supplied. Waiting to fetch from topic '%s/camera_info'", camera_ns.c_str());
+      auto camera_info = ros::topic::waitForMessage<sensor_msgs::CameraInfo>(camera_ns + "/camera_info", nh);
+      if(!camera_info)
+      {
+        ROS_INFO("Interrupted by node shutdown.");
+        exit(0);
+      }
+      if(std::all_of(camera_info->K.begin(), camera_info->K.end(), [](double k) { return k == 0;}))
+      {
+        ROS_WARN("Intrinsics of CameraInfo fetched from topic '%s/camera_info' are all zero."
+                 " Make sure your camera publishes the correct intrinsics", camera_ns.c_str());
+      }
+      /* sensor_msgs/CameraInfo only describes contents of intrinsic camera matrix in comments in msg file, so
+         there is no point having named constants here, sorry. */
+      camera_focal_length_x_px_ = camera_info->K[0];
+      camera_focal_length_y_px_ = camera_info->K[4];
+      camera_principal_point_x_px_ = camera_info->K[2];
+      camera_principal_point_y_px_ = camera_info->K[5];
+    }
 
     ROS_INFO_STREAM("got focal length " << camera_focal_length_x_px_ << ", " << camera_focal_length_y_px_);
     ROS_INFO_STREAM("got principal point " << camera_principal_point_x_px_ << ", " << camera_principal_point_y_px_);
-    ROS_INFO_STREAM("got tag size (in meters) " << tag_size_m_);
+
+    image_sub_ = it_.subscribe(camera_ns + "/image_raw", 1, &AprilTagNode::imageCb, this);
+    tag_list_pub_ = nh.advertise<april_tag::AprilTagList>("/april_tags", 100);
+
+    // be run simultaneously while using different parameters.
+    private_node_handle.param<bool>("show_debug_image", show_debug_image_, false);
 
     if (show_debug_image_)
     {
