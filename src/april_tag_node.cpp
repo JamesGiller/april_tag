e5f5b93dec9039209a652c01cd13e46b96cb80b0
James Giller <giller@bubblelab.com>
2017-10-13 15:38:31 +0900

    Extract detection of tags in ROS images.

--- src/april_tag_node.cpp
+++ src/april_tag_node.cpp
@@ -2,43 +2,35 @@
 // adapted from ros example and april tag examples - palash
 //
 #include <algorithm>
+#include <functional>
+#include <iterator>
 
-#include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <ros/ros.h>
 #include <sensor_msgs/CameraInfo.h>
-#include <sensor_msgs/image_encodings.h>
 
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
+#include "april_tag/detect_tags_in_ros_image.h"
 #include "april_tag/tag_detection_to_msg.h"
-#include "april_tag/AprilTag.h" // rosmsg
-#include "april_tag/AprilTagList.h" // rosmsg
-#include "AprilTags/TagDetector.h"
+#include "april_tag/AprilTagList.h"
 #include "AprilTags/Tag36h11.h"
 
 class AprilTagNode
 {
-  static const std::string OPENCV_WINDOW;
-
   // allow configurations for these:  
-  bool  show_debug_image_;
-  sensor_msgs::ImageConstPtr latest_image_;
   double camera_focal_length_x_px_;
   double camera_focal_length_y_px_;
   double camera_principal_point_x_px_;
   double camera_principal_point_y_px_;
   double tag_size_m_;
 
-  AprilTags::TagDetector tag_detector_;
+  april_tag::DetectTagsInROSImage detect_tags_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   ros::Publisher tag_list_pub_;
   
 public:
   AprilTagNode(ros::NodeHandle &nh, const AprilTags::TagCodes &tag_codes) : 
-    tag_detector_(tag_codes),
+    detect_tags_(tag_codes),
     it_(nh) 
   {
     double tag_size_cm;
@@ -91,82 +83,37 @@ public:
 
     image_sub_ = it_.subscribe(camera_ns + "/image_raw", 1, &AprilTagNode::imageCb, this);
     tag_list_pub_ = nh.advertise<april_tag::AprilTagList>("/april_tags", 100);
-
-    // be run simultaneously while using different parameters.
-    private_node_handle.param<bool>("show_debug_image", show_debug_image_, false);
-
-    if (show_debug_image_)
-    {
-      cv::namedWindow(OPENCV_WINDOW);
-    }
   }
 
-  ~AprilTagNode()
+  april_tag::AprilTag convertToMsg(const AprilTags::TagDetection &detection, const sensor_msgs::ImageConstPtr &in_img)
   {
-    if (show_debug_image_)
-    {
-     cv::destroyWindow(OPENCV_WINDOW);
-    }
-  }
-
-  april_tag::AprilTag convert_to_msg(AprilTags::TagDetection &detection) {
-    april_tag::DetectionContext detection_context = {latest_image_->header,
+    april_tag::DetectionContext detection_context = {in_img->header,
                                                      camera_focal_length_x_px_, camera_focal_length_y_px_,
                                                      camera_principal_point_x_px_, camera_principal_point_y_px_,
                                                      tag_size_m_};
     return april_tag::tagDetectionToMsg(detection, detection_context);
   }
 
-  void processCvImage(cv_bridge::CvImagePtr cv_ptr) 
+  void imageCb(const sensor_msgs::ImageConstPtr &msg)
   {
-    cv::Mat image_gray;
-    cv::cvtColor(cv_ptr->image, image_gray, CV_BGR2GRAY);
-    vector<AprilTags::TagDetection> detections = tag_detector_.extractTags(image_gray);
-    vector<april_tag::AprilTag> tag_msgs;
-
-    for (int i=0; i < detections.size(); ++i)
-    {
-      if (show_debug_image_)
-      {
-        detections[i].draw(cv_ptr->image);
-      }
-      tag_msgs.push_back(convert_to_msg(detections[i]));
-    }
-
-    if(detections.size() > 0)
-    { // take this out if you want absence notificaiton
-      april_tag::AprilTagList tag_list;
-      tag_list.april_tags = tag_msgs;
-      tag_list_pub_.publish(tag_list);
-    }
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    latest_image_ = msg;
-    cv_bridge::CvImagePtr cv_ptr;
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
+      auto detections = detect_tags_(msg);
 
-    processCvImage(cv_ptr);
-
-    if (show_debug_image_)
+      if(detections.size() > 0)
+      {
+        april_tag::AprilTagList tag_list;
+        std::transform(detections.begin(), detections.end(), std::back_inserter(tag_list.april_tags),
+                       std::bind(&AprilTagNode::convertToMsg, this, std::placeholders::_1, msg));
+        tag_list_pub_.publish(tag_list);
+      }
+    }
+    catch (const std::runtime_error& e)
     {
-      // Update GUI Window
-      cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-      cv::waitKey(3);
+      ROS_ERROR("%s", e.what());
     }
   }
 };
-
-const std::string AprilTagNode::OPENCV_WINDOW = "Image window";
 
 int main(int argc, char** argv)
 {
